a9089bd06c8e79ae4c26156582e0cd917d24e84d
Joshua Gonzalez <jg@jtmg.io>
2018-10-06 10:22:24 -0400

    initial commit

---
+++ src/app/dashboard.config.ts
@@ -0,0 +1,150 @@
+/*
+		{
+			page: '',
+			icon: 'md-',
+			title: '',
+		},
+
+*/
+export const venueMaps = [
+	{
+		title: 'View Caesars Palace Upper Level Map',
+		map:
+			'https://mobile.eventready.com/files/york/Sheraton-upper-level-2017.jpg',
+	},
+	{
+		title: 'View Caesars Palace Lower Level Map',
+		map:
+			'https://mobile.eventready.com/files/york/Sheraton-lower-level-2017.jpg',
+	},
+];
+export const banners = [
+	{
+		title: 'Event Ready Conference',
+		banner: './assets/banner-3.jpg',
+	},
+	{
+		title: 'Event Ready Conference',
+		banner: './assets/banner-2.jpg',
+	},
+	{
+		title: 'Event Ready Conference',
+		banner: './assets/banner-1.jpg',
+	},
+];
+export const attendeeMenu = {
+	featureRow: [
+		{
+			page: 'InfoBoothPage',
+			icon: 'md-information-circle',
+			title: 'Info',
+		},
+		{
+			page: 'SpeakersPage',
+			icon: 'md-school',
+			title: 'Speakers',
+		},
+		{
+			page: 'VenuesPage',
+			icon: 'md-image',
+			title: 'Venue',
+		},
+		{
+			page: 'SponsorsPage',
+			icon: 'md-people',
+			title: 'Sponsors',
+		},
+	],
+	row1: [
+		{
+			page: 'WhatsOnPage',
+			icon: 'md-alarm',
+			title: 'What&apos;s On',
+		},
+		{
+			page: 'SchedulePage',
+			icon: 'md-calendar',
+			title: 'Schedule',
+		},
+		{
+			page: 'MyProfilePage',
+			icon: 'md-person',
+			title: 'My Profile',
+		},
+	],
+	row2: [
+		{
+			page: 'MySchedulePage',
+			icon: 'md-list-box',
+			title: 'My Schedule',
+		},
+		{
+			page: 'SurveysPage',
+			icon: 'md-people',
+			title: 'My Surveys',
+		},
+		{
+			page: 'MyExhibitorsPage',
+			icon: 'md-contact',
+			title: 'My Exhibitors',
+		},
+	],
+	row3: [
+		{
+			page: 'ExhibitorsPage',
+			icon: 'md-contacts',
+			title: 'Exhibitors',
+		},
+		{
+			page: 'YoutubePage',
+			icon: 'logo-youtube',
+			title: 'Videos',
+		},
+		{
+			page: 'TwitterPage',
+			icon: 'logo-twitter',
+			title: 'Twitter',
+		},
+	],
+};
+export const speakerMenu = {
+	featureRow: [
+		{
+			page: 'SpeakerSessionsPage',
+			icon: 'md-list-box',
+			title: 'My Sessions',
+		},
+		{
+			page: 'VenuesPage',
+			icon: 'md-image',
+			title: 'Venue',
+		},
+		{
+			page: 'InfoBoothPage',
+			icon: 'md-information-circle',
+			title: 'Info',
+		},
+		{
+			page: 'SponsorsPage',
+			icon: 'md-people',
+			title: 'Sponsors',
+		},
+	],
+	row1: [
+		{
+			page: 'WhatsOnPage',
+			icon: 'md-alarm',
+			title: 'What&apos;s On',
+		},
+		{
+			page: 'YoutubePage',
+			icon: 'logo-youtube',
+			title: 'Youtube',
+		},
+		{
+			page: 'TwitterPage',
+			icon: 'logo-twitter',
+			title: 'Twitter',
+		},
+	],
+};
---
+++ src/app/globals.ts
@@ -0,0 +1,49 @@
+export const eventUrl: string =
+	'https://api.eventready.com/eventready/index.cfm?fuseaction=api';
+export const apiUrl: string = 'https://api.eventready.com/eventready/';
+export const downloadUrl: string =
+	'https://mobile.eventready.com/files/eventready/';
+export const eventName: string = 'Event Ready';
+export const eventDesc: string = 'Event Ready';
+export const eventId: number = 205;
+export const clientId: number = 63;
+export const locationId: number = 208;
+export const database: string = 'iplan-prime';
+export const cl: Function = console.log.bind(window.console);
+export const EMAIL_REGEXP: RegExp = /^[a-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-z0-9]([a-z0-9-]*[a-z0-9])?(\.[a-z0-9]([a-z0-9-]*[a-z0-9])?)*$/i;
+export const serverToken: string =
+	'8C022ABDF84DCE2754472AC34892DBD7A2F36107EC0E6EC05FE37A7784BBCDD1';
+export const oneSignalKey: string = '8c3f3e74-7676-4a00-8001-be2422bc89ec';
+export const googleAnalyticsKey: string = 'UA-126733811-1';
+export const facebook: string = 'https://www.facebook.com';
+export const twitter: string = 'https://twitter.com';
+export const youtube: string = 'http://www.youtube.com';
+export const aboutConference: string = 'http://eventready.com';
+export const hasSpeakerLogin: boolean = true;
+export const appEmail: string = 'info@eventready.com';
+export const appId: string = 'com.eventready.eventreadyapp2018';
+export const isDemo: boolean = true;
+export const defaultProfilePic: string =
+	'https://mobile.eventready.com/files/eventready/nobody_m.original.gif';
+export const speakerDownloadURl: string =
+	'https://iplan-prime.eventready.com/docs/download/photo_files/';
+
+export const surveyMessage: string =
+	'Below you will find survey questions about the Event Ready conference. Please answer each question to the best of your ability.';
+
+export const dashboardBannerHeight: string = '150px';
+export const dashboardBannerSpeed: number = 3500;
+export const scheduleDays: any[] = [
+	{
+		day: 'Tues',
+		tab: 'ScheduleTabDay1Page',
+	},
+	{
+		day: 'Weds',
+		tab: 'ScheduleTabDay2Page',
+	},
+	{
+		day: 'Thurs',
+		tab: 'ScheduleTabDay3Page',
+	},
+];
